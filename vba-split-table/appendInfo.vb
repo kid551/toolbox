90794728779d7413c7849d7bc503b5bc8b7cb3d3
Tao Xie <xietao26@gmail.com>
2017-05-04 21:24:43 +0800

    Complete the dictionary elements.

--- vba-split-table/appendInfo.vb
+++ vba-split-table/appendInfo.vb
@@ -18,8 +18,32 @@ End Sub
 Function getColorDict() As Object
     Set getColorDict = CreateObject("Scripting.Dictionary")
     
+    getColorDict.Add "501", "1芥黄"
+    getColorDict.Add "502", "2土黄"
     getColorDict.Add "503", "3军绿"
     getColorDict.Add "504", "4浅卡"
+    getColorDict.Add "505", "5杏色"
+    getColorDict.Add "506", "6浅灰"
+    getColorDict.Add "508", "8军黄"
+    getColorDict.Add "509", "9深军"
+    getColorDict.Add "510", "10深灰"
+    getColorDict.Add "511", "11深藏青"
+    getColorDict.Add "512", "12深卡其"
+    getColorDict.Add "514", "14米白"
+    getColorDict.Add "601", "601"
+    getColorDict.Add "602", "602"
+    getColorDict.Add "603", "603卡其"
+    getColorDict.Add "604", "604深卡其"
+    getColorDict.Add "605", "605绿灰"
+    getColorDict.Add "606", "606深卡其"
+    getColorDict.Add "607", "607深灰"
+    getColorDict.Add "608", "608"
+    getColorDict.Add "609", "609"
+    getColorDict.Add "610", "610"
+    getColorDict.Add "611", "611"
+    getColorDict.Add "612", "612"
+    getColorDict.Add "灰杏", "灰杏"
+    getColorDict.Add "乔雄使用颜色", "乔雄使用颜色"
 End Function
 
 Function getLastRowIndx(ByVal sheetName)
