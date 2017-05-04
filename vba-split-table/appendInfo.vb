0d092f4393df09da3b7436f9319a88debb488b02
Tao Xie <xietao26@gmail.com>
2017-05-04 17:08:20 +0800

    Refactor code.

--- vba-split-table/appendInfo.vb
+++ vba-split-table/appendInfo.vb
@@ -13,21 +13,28 @@ Sub appendInfo()
     Sheets(1).Range(currRowToLast).Copy Sheets("3¾üÂÌ").Range(stG3Empty)
 End Sub
 
-
-
 Sub getSubName()
-    Dim currCellRow, st1Lst
-    currCellRow = Selection.Row
-    st1Lst = Sheets(1).Range("a65536").End(3).Row
+    ' Create color code dictionary
+    Dim colorDict As Object
+    Set colorDict = CreateObject("Scripting.Dictionary")
     
-    ' Get region of current cell to last column "o" cell.
-    currRowToLast = "a" & currCellRow & ":o" & st1Lst
+    colorDict("503") = "3?¨¹?¨¬"
+    colorDict("504") = "4?3?¡§"
     
-    Dim cRegion As Range
-    Set cRegion = Sheets(1).Range(currRowToLast)
+    currRow = Selection.Row
+    sheet1LastRow = Sheets(1).Range("a65536").End(3).Row
+    
+    ' Get region string of current cell to last column "o" cell.
+    addedRegionStr = "a" & currRow & ":o" & sheet1LastRow
+    
+    Dim addedRegion As Range
+    Set addedRegion = Sheets(1).Range(addedRegionStr)
         
-    For Each pRow In cRegion.Rows
-        MsgBox "dd " & Right(pRow.Cells(2), 3)
+    For Each iRow In addedRegion.Rows
+        corrSheetName = colorDict(Right(iRow.Cells(2), 3))
+        corrSheetFirstEmpty = (Sheets(corrSheetName).Range("a65536").End(3).Row + 1)
+                
+        iRow.Copy Sheets(corrSheetName).Range("a" & corrSheetFirstEmpty)
     Next
     
 End Sub
