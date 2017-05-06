8e112f867c489b5fe1d16398761bd4b8b1e6be49
Tao Xie <xietao26@gmail.com>
2017-05-06 17:39:52 +0800

    Abstract 'build sell row' procedure.

--- vba-split-table/appendInfo.vb
+++ vba-split-table/appendInfo.vb
@@ -2,34 +2,34 @@ Function getColorDict() As Object
     Set getColorDict = CreateObject("Scripting.Dictionary")
     
     getColorDict.Add "501", "1????"
-    getColorDict.Add "502", "2¨ª¨¢??"
-    getColorDict.Add "503", "3?¨¹?¨¬"
-    getColorDict.Add "504", "4?3?¡§"
-    getColorDict.Add "505", "5D¨®¨¦?"
-    getColorDict.Add "506", "6?3?¨°"
-    getColorDict.Add "508", "8?¨¹??"
-    getColorDict.Add "509", "9¨¦??¨¹"
-    getColorDict.Add "510", "10¨¦??¨°"
-    getColorDict.Add "511", "11¨¦?2??¨¤"
-    getColorDict.Add "512", "12¨¦??¡§??"
-    getColorDict.Add "514", "14?¡Á¡ã¡Á"
+    getColorDict.Add "502", "2??????"
+    getColorDict.Add "503", "3??????"
+    getColorDict.Add "504", "4?3???"
+    getColorDict.Add "505", "5D?????"
+    getColorDict.Add "506", "6?3???"
+    getColorDict.Add "508", "8?????"
+    getColorDict.Add "509", "9??????"
+    getColorDict.Add "510", "10??????"
+    getColorDict.Add "511", "11???2????"
+    getColorDict.Add "512", "12????????"
+    getColorDict.Add "514", "14??????"
     getColorDict.Add "601", "601"
     getColorDict.Add "602", "602"
-    getColorDict.Add "603", "603?¡§??"
-    getColorDict.Add "604", "604¨¦??¡§??"
-    getColorDict.Add "605", "605?¨¬?¨°"
-    getColorDict.Add "606", "606¨¦??¡§??"
-    getColorDict.Add "607", "607¨¦??¨°"
+    getColorDict.Add "603", "603?????"
+    getColorDict.Add "604", "604????????"
+    getColorDict.Add "605", "605??????"
+    getColorDict.Add "606", "606????????"
+    getColorDict.Add "607", "607??????"
     getColorDict.Add "608", "608"
     getColorDict.Add "609", "609"
     getColorDict.Add "610", "610"
     getColorDict.Add "611", "611"
     getColorDict.Add "612", "612"
-    getColorDict.Add "?¨°D¨®", "?¨°D¨®"
-    getColorDict.Add "??D?¨º1¨®???¨¦?", "??D?¨º1¨®???¨¦?"
+    getColorDict.Add "???D??", "???D??"
+    getColorDict.Add "??D???1????????", "??D???1????????"
     
-    getColorDict.Add "C32", "32¡Á¨¹?a¡ä?"
-    getColorDict.Add "C16", "16¡Á¨¹?a¡ä?"
+    getColorDict.Add "C32", "32?????a???"
+    getColorDict.Add "C16", "16?????a???"
 End Function
 
 Function getLastRowIndx(ByVal wbName, ByVal sheetName)
@@ -97,6 +97,24 @@ Function getCellContents(ByVal wbName, ByVal stName, ByVal cPos) As Range
     Set getCellContents = Workbooks(wbName).Sheets(stName).Range(cPos)
 End Function
 
+Sub buildSellRow(ByVal copiedRow, ByVal wbName, ByVal sheetName, ByVal unitPrice)
+    firstDomain = "a:e"
+    secondDomain = "h"
+    thirdDomain = "i"
+    fourthDomain = "l"
+
+    Set copiedRange = Union(copiedRow.Columns(firstDomain), copiedRow.Columns(secondDomain), copiedRow.Columns(thirdDomain), copiedRow.Columns(fourthDomain))
+    
+    ctRowStartIndx = getLastRowIndx(wbName, sheetName) + 1
+    Call copyRowToSheet(copiedRange, wbName, sheetName)
+                
+    Workbooks(wbName).Sheets(sheetName).Cells(ctRowStartIndx, 9) = "=H" & ctRowStartIndx & "*G" & ctRowStartIndx
+    Workbooks(wbName).Sheets(sheetName).Cells(ctRowStartIndx, 10) = unitPrice
+    Workbooks(wbName).Sheets(sheetName).Cells(ctRowStartIndx, 11) = "=J" & ctRowStartIndx & "*I" & ctRowStartIndx
+    Workbooks(wbName).Sheets(sheetName).Cells(ctRowStartIndx, 15) = "=o" & CStr(ctRowStartIndx - 1) & "+K" & ctRowStartIndx & "-M" & ctRowStartIndx & "-N" & ctRowStartIndx
+    
+End Sub
+
 Sub copyToWorkBook()
     controlCenterWBName = "?????DD?.xlsm"
     controlCenterMainSheetName = 1
@@ -107,6 +125,11 @@ Sub copyToWorkBook()
     ccCTNameCell = "b5"
     ccUnitPriceCell = "b6"
     
+    whMainSTName = 1
+    whLstColIndx = "o"
+    ctMainSTName = 1
+    ctColStartIndx = 1
+    
     ' ***********************
     
     warehouseWBName = getCellContents(controlCenterWBName, controlCenterMainSheetName, ccWHNameCell)
@@ -114,26 +137,11 @@ Sub copyToWorkBook()
     
     customerWBName = getCellContents(controlCenterWBName, controlCenterMainSheetName, ccCTNameCell)
     unitPrice = getCellContents(controlCenterWBName, controlCenterMainSheetName, ccUnitPriceCell)
-            
-    whMainSTName = 1
-    whLstColIndx = "o"
-    ctMainSTName = 1
-    ctColStartIndx = 1
     
     
-    Dim copiedRange As Range
     For Each iRow In getAddedRegion(warehouseWBName, whMainSTName, whLstColIndx, warehouseStartPos).Rows
         If iRow.Columns("c") = "???" Then
-            Set copiedRange = Union(iRow.Columns("a:e"), iRow.Columns("h"), iRow.Columns("i"), iRow.Columns("l"))
-            
-            ctRowStartIndx = getLastRowIndx(customerWBName, ctMainSTName) + 1
-            Call copyRowToSheet(copiedRange, customerWBName, ctMainSTName)
-                        
-            Workbooks(customerWBName).Sheets(ctMainSTName).Cells(ctRowStartIndx, 9) = "=H" & ctRowStartIndx & "*G" & ctRowStartIndx
-            Workbooks(customerWBName).Sheets(ctMainSTName).Cells(ctRowStartIndx, 10) = unitPrice
-            Workbooks(customerWBName).Sheets(ctMainSTName).Cells(ctRowStartIndx, 11) = "=J" & ctRowStartIndx & "*I" & ctRowStartIndx
-            Workbooks(customerWBName).Sheets(ctMainSTName).Cells(ctRowStartIndx, 15) = "=o" & CStr(ctRowStartIndx - 1) & "+K" & ctRowStartIndx & "-M" & ctRowStartIndx & "-N" & ctRowStartIndx
-                        
+            Call buildSellRow(iRow, customerWBName, ctMainSTName, unitPrice)
         End If
     Next
     
